49368d45457f8ec5974432574834f6459a87b917
Lee Houk <leehouk@gmail.com>
2023-05-30 11:54:01 -0700

    updating JSON

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const data = {
-    imgPath: "./somewhere/img.",
-    imgAltText: "Image of a cat",
-    title: "Cat",
-    actionlink: "https://localhost:5500/games/game.html"
+    brandName: "Smash Games",
+    pages: [
+        {
+        name: "index",
+        pageName: "Home",
+        callToAction: {
+            src: "images/SmashGamesLogo-short.png",
+            altText: "Smash Games Logo"
+        }
+
+    },
+    {
+        name: "blast",
+        pageName: "Inferno Blast",
+        callToAction: {
+            src: "images/Inferno-Jumbotron.png",
+            altText: "Inferno Blast Logo",
+            buttonLinkSrc:"https://steampowered.com",
+            buttonLinkText:"Buy Now"
+        },
+    }
+    ]
 }
